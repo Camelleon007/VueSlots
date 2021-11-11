24b5683b6a003bc662ad25d504dcea8dbc9ebfb6
Kamil Kowalski <kamil@olicom.pl>
2021-11-11 14:20:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <the-header></the-header> -->
+  <TheHeader />
+  <BadgeList />
+  <UserInfo
+    :full-name="activeUser.name"
+    :info-text="activeUser.description"
+    :role="activeUser.role"
+  />
+  <CourseGoals>
+    <template #default="goalsProps">
+      <h2>{{ goalsProps.item }}</h2>
+      <p>{{ goalsProps["item-key"] }}</p>
+    </template>
+  </CourseGoals>
+  <CourseGoals>
+    <template #default="goalsProps">
+      <h1 class="color-red">{{ goalsProps.item }}</h1>
+      <p class="bg-green">Desc: {{ goalsProps.itemDesc }}</p>
+    </template>
+  </CourseGoals>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
+export default {
+  components: {
+    //"the-header": TheHeader,
+    //TheHeader: TheHeader,
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+.color-red {
+  color: red;
+}
+.bg-green {
+  background-color: greenyellow;
+}
+</style>
---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <base-card>
+      <template v-slot:header>
+        <h3>Course goals:</h3>
+      </template>
+      <ul>
+        <li v-for="goal in goals" :key="goal.id">
+          <slot :item="goal" itemDesc="Lorem.impsum"></slot>
+        </li>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Finish the course", "Learn Vue.js"],
+    };
+  },
+  mounted() {
+    console.log(this.$slots);
+  },
+};
+</script>
